e9bdc4a9c919ec4809ccdcb981ebd143aa7b3719
xhendor <rosendorsc@gmail.com>
2020-01-13 19:37:15 -0800

    Initial commit

---
+++ app/src/main/java/com/uabc/edu/sqlite/DatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.uabc.edu.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    //Nombre de la tabla
+    public static final String TABLE_NAME="COUNTRIES";
+    //Columnas
+    public static final String _ID="_id";
+    public static final String SUBJECT="subject";
+    public static final String DESC="description";
+
+    //Database NAME
+    public static final String DB_NAME="DEV_COUNTRIES.DB";
+
+    //version
+    static final int DB_VERSION= 1;
+
+    private final String CREATE_TABLE="create table "+ TABLE_NAME +
+            " ("+ _ID +" INTEGER PRIMARY KEY AUTOINCREMENT, "+ SUBJECT+
+            "TEXT NOT NULL,"+ DESC +" TEXT)";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db,
+                          int oldVersion,
+                          int newVersion) {
+        //RESPALDAR y DESPUES BORRAR
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        onCreate(db);
+    }
+}
